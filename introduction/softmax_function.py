9043e830b087990c96aaf42193beeb64283d7327
Sherlock <miguel.saaruiz@gmail.com>
2020-04-19 15:18:15 -0500

    Softmax function implementation in Python

---
+++ introduction/softmax_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+def softmax(L):
+    expL = np.exp(L)
+    return np.divide (expL, expL.sum())
